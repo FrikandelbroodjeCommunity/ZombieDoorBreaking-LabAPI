4b2a889e7f0b0dd27b7009de0eb30ac61e04352c
SebasCapo <sebascapoxo@gmail.com>
2022-01-25 20:23:02 -0300

    version bump

--- StrongerZombies/StrongerZombies.cs
+++ StrongerZombies/StrongerZombies.cs
@@ -17,7 +17,7 @@ namespace StrongerZombies
         public override string Name { get; } = "StrongerZombies";
         public override string Author { get; } = "Beryl";
         public override string Prefix { get; } = "strongerzombies";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(2, 0, 0);
         public override Version RequiredExiledVersion => new Version(4, 2, 3);
 
         private ZombieHandler handlers;
