1ff701d92e66ca9d9fd064bbaa0d6dd02db027e4
Snivy FIlms <120346554+NikkiGardiner1@users.noreply.github.com>
2024-04-19 11:09:40 -0400

    Version Bump (I keep forgetting this)

--- StrongerZombies/StrongerZombies.cs
+++ StrongerZombies/StrongerZombies.cs
@@ -17,7 +17,7 @@ namespace StrongerZombies
         public override string Name { get; } = "StrongerZombies";
         public override string Author { get; } = "Beryl, Vicious Vikki";
         public override string Prefix { get; } = "strongerzombies";
-        public override Version Version { get; } = new Version(2, 1, 0);
+        public override Version Version { get; } = new Version(2, 1, 1);
         public override Version RequiredExiledVersion => new Version(8, 8, 1);
 
         private ZombieHandler handlers;
